b9a7f9ad9b83ffe8e5fbe65ed800bc0574775640
dbunin <bunind@live.com>
2016-02-18 11:41:28 +0200

    Added list and timer

--- Week 1/Week 1/Form1.Designer.cs
+++ Week 1/Week 1/Form1.Designer.cs
@@ -28,116 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.components = new System.ComponentModel.Container();
+            this.rbFirstInFirstOut = new System.Windows.Forms.RadioButton();
+            this.rbShortestSeekTime = new System.Windows.Forms.RadioButton();
+            this.rbSCANDisc = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // rbFirstInFirstOut
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 24);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(127, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "First Come First Serve";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rbFirstInFirstOut.AutoSize = true;
+            this.rbFirstInFirstOut.Location = new System.Drawing.Point(9, 37);
+            this.rbFirstInFirstOut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbFirstInFirstOut.Name = "rbFirstInFirstOut";
+            this.rbFirstInFirstOut.Size = new System.Drawing.Size(191, 24);
+            this.rbFirstInFirstOut.TabIndex = 0;
+            this.rbFirstInFirstOut.TabStop = true;
+            this.rbFirstInFirstOut.Text = "First Come First Serve";
+            this.rbFirstInFirstOut.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // rbShortestSeekTime
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(6, 47);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(140, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Shortest Seek Time First";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rbShortestSeekTime.AutoSize = true;
+            this.rbShortestSeekTime.Location = new System.Drawing.Point(9, 72);
+            this.rbShortestSeekTime.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbShortestSeekTime.Name = "rbShortestSeekTime";
+            this.rbShortestSeekTime.Size = new System.Drawing.Size(209, 24);
+            this.rbShortestSeekTime.TabIndex = 1;
+            this.rbShortestSeekTime.TabStop = true;
+            this.rbShortestSeekTime.Text = "Shortest Seek Time First";
+            this.rbShortestSeekTime.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // rbSCANDisc
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(6, 69);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(123, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "SCAN disc (elevator)";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.rbSCANDisc.AutoSize = true;
+            this.rbSCANDisc.Location = new System.Drawing.Point(9, 106);
+            this.rbSCANDisc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbSCANDisc.Name = "rbSCANDisc";
+            this.rbSCANDisc.Size = new System.Drawing.Size(180, 24);
+            this.rbSCANDisc.TabIndex = 2;
+            this.rbSCANDisc.TabStop = true;
+            this.rbSCANDisc.Text = "SCAN disc (elevator)";
+            this.rbSCANDisc.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Location = new System.Drawing.Point(178, 12);
+            this.groupBox1.Controls.Add(this.rbFirstInFirstOut);
+            this.groupBox1.Controls.Add(this.rbSCANDisc);
+            this.groupBox1.Controls.Add(this.rbShortestSeekTime);
+            this.groupBox1.Location = new System.Drawing.Point(267, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(150, 100);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(225, 154);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "strategy";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(194, 131);
+            this.button1.Location = new System.Drawing.Point(291, 202);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 4;
             this.button1.Text = "run";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(194, 173);
+            this.button2.Location = new System.Drawing.Point(291, 266);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 5;
             this.button2.Text = "stop";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Location = new System.Drawing.Point(138, 18);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(73, 20);
+            this.textBox1.Size = new System.Drawing.Size(108, 26);
             this.textBox1.TabIndex = 6;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(92, 48);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(138, 74);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(73, 186);
+            this.listBox1.Size = new System.Drawing.Size(108, 284);
             this.listBox1.TabIndex = 7;
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(13, 7);
+            this.trackBar1.Location = new System.Drawing.Point(20, 11);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar1.Size = new System.Drawing.Size(45, 231);
+            this.trackBar1.Size = new System.Drawing.Size(69, 347);
             this.trackBar1.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 267);
+            this.ClientSize = new System.Drawing.Size(526, 411);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -150,15 +166,16 @@
 
         #endregion
 
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton rbFirstInFirstOut;
+        private System.Windows.Forms.RadioButton rbShortestSeekTime;
+        private System.Windows.Forms.RadioButton rbSCANDisc;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Week 1/Week 1/Form1.cs
+++ Week 1/Week 1/Form1.cs
@@ -12,9 +12,53 @@ namespace Week_1
 {
     public partial class Form1 : Form
     {
+        List<int> myList = new List<int>();
+        List<int> sortedList = new List<int>();
         public Form1()
         {
             InitializeComponent();
+            myList.Add(40);
+            myList.Add(30);
+            myList.Add(80);
+            myList.Add(86);
+            myList.Add(54);
+            myList.Add(27);
+            myList.Add(46);
+            myList.Add(2);
+            myList.Add(99);
+            myList.Add(1);
+            myList.Add(14);
+            myList.Add(72);
+            DisplayList();
+        }
+
+        void DisplayList()
+        {
+            foreach(int i in myList)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            if (rbFirstInFirstOut.Checked)
+            {
+                for(int i = myList.Count() - 1; i > -1; i++)
+                {
+                    sortedList.Add(myList[i]);
+                }
+            }
+            else
+                if (rbShortestSeekTime.Checked)
+                {
+                    
+                }
+                else
+                {
+
+                }
         }
     }
 }
